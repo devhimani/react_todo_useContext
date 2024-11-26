8569f944b0f9e2b6f515e7d95a4d3e815ddea2cb
himanigupta <gupta.himani44@gmail.com>
2024-11-26 14:40:20 -0800

    creates routes & pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home.";
+import Todo from "./pages/Todo";
 
 function App() {
   return (
     <>
-      <div> Hello App.js</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/todo" element={<Todo />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
