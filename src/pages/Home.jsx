8f402e6e92e0b48b28da25dd83f583986f4163ec
himanigupta <gupta.himani44@gmail.com>
2024-11-29 06:30:01 -0800

    edit todo and update, delete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,18 @@ import { TodoStore } from "../store/index.jsx";
 
 const Home = () => {
   const { todos } = useContext(TodoStore);
-
+  console.log(todos);
   return (
     <div>
       <Link to="/todo">Todo</Link>
       <div>
         <h1> Totoal {todos.length} tasks</h1>
-        <h3>05 pending tasks</h3>
-        <h4>05 completed tasks</h4>
+        <h3>
+          pending tasks {todos.filter((todo) => !todo.isCheckCompleted).length}
+        </h3>
+        <h4>
+          completed tasks {todos.filter((todo) => todo.isCheckCompleted).length}
+        </h4>
       </div>
     </div>
   );
--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,17 +1,49 @@
 import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { TodoStore } from "../store/index.jsx";
+import "./Todo.css";
 
 const Todo = () => {
   const { todos, setTodos } = useContext(TodoStore);
   const [input, setInput] = useState("");
 
+  const [editIndex, setEditIndex] = useState(null);
+  const [newInput, setNewInput] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    setTodos([...todos, input]);
+    setTodos([
+      ...todos,
+      { id: Date.now(), name: input, isCheckCompleted: false },
+    ]);
     setInput("");
+    console.log("form submitted");
   };
+
+  const handleDelete = (clickedIndex) => {
+    setTodos(todos.filter((_todo, index) => clickedIndex !== index));
+  };
+
+  const handleUpdateTodo = (clickedIndex) => {
+    setTodos(
+      todos.map((todo, index) =>
+        clickedIndex === index ? { ...todo, name: newInput } : todo
+      )
+    );
+    setEditIndex(null);
+  };
+
+  const handleDone = (clickedIndex) => {
+    setTodos(
+      todos.map((todo, index) =>
+        clickedIndex === index
+          ? { ...todo, isCheckCompleted: !todo.isCheckCompleted }
+          : todo
+      )
+    );
+  };
+
   return (
     <div>
       <Link to="/">Home</Link>
@@ -24,12 +56,50 @@ const Todo = () => {
             value={input}
             onChange={(e) => setInput(e.target.value)}
           />
-          <button type="submit">Add</button>
+          <button>Add</button>
         </form>
         <div>
           <ul>
             {todos.map((todo, index) => (
-              <li key={index}> {todo}</li>
+              <li key={index} className="todo-item">
+                {editIndex === index ? (
+                  <input
+                    type="text"
+                    value={newInput}
+                    onChange={(e) => setNewInput(e.target.value)}
+                  />
+                ) : (
+                  <div
+                    className={`todo-checkbox ${
+                      todo.isCheckCompleted ? "done" : ""
+                    }`}
+                  >
+                    <input
+                      type="checkbox"
+                      checked={todo.isCheckCompleted}
+                      onChange={() => handleDone(index)}
+                    />
+                    <p>{todo.name}</p>
+                  </div>
+                )}
+                <div>
+                  {editIndex === index ? (
+                    <button onClick={() => handleUpdateTodo(index)}>
+                      Update
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => {
+                        setEditIndex(index);
+                        setNewInput(todo.name);
+                      }}
+                    >
+                      Edit
+                    </button>
+                  )}
+                  <button onClick={() => handleDelete(index)}>Delete</button>
+                </div>
+              </li>
             ))}
           </ul>
         </div>
